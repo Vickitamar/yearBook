c6eeb22e1dc771c4159f85d4e4924f29f72a751c
Vicki Edwards <vicki.edwards@quadrotech-it.com>
2019-08-27 12:57:39 +0100

    added in repsonsiveness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { Gallery } from "./components/Gallery";
+import { Modal } from "./components/Modal";
+import Books from "./components/Books";
 
 class ModalSwitch extends Component {
   previousLocation = this.props.location;
@@ -39,26 +42,18 @@ class ModalSwitch extends Component {
   }
 }
 
-const Image = styled.div`
-  width: 400px;
-  height: 400px;
+export const Image = styled.div`
+  width: 305px;
+  height: 305px;
   background: no-repeat center/150% url(/img/${({ index }) => index}.jpeg);
+  ${({ inModal }) =>
+    !inModal &&
+    css`
+      : hover {
+        opacity: 0.7;
+      }
+    `}
 `;
-
-const IMAGES = [
-  { id: 1, title: "Dark Orchid", color: "DarkOrchid" }, // get rid of colour prop
-  { id: 2, title: "Lime Green", color: "LimeGreen" },
-  { id: 3, title: "Tomato", color: "Tomato" },
-  { id: 4, title: "Seven Ate Nine", color: "#789" },
-  { id: 5, title: "Crimson", color: "Crimson" },
-  { id: 6, title: "Dark Orchid", color: "DarkOrchid" },
-  { id: 7, title: "Lime Green", color: "LimeGreen" },
-  { id: 8, title: "Tomato", color: "Tomato" },
-  { id: 9, title: "Seven Ate Nine", color: "#789" },
-  { id: 10, title: "Crimson", color: "Crimson" },
-  { id: 11, title: "Seven Ate Nine", color: "#789" },
-  { id: 12, title: "Crimson", color: "Crimson" }
-];
 
 function Home() {
   return (
@@ -77,32 +72,8 @@ function Home() {
   );
 }
 
-const PhotoGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 305px);
-`;
-
-function Gallery() {
-  return (
-    <PhotoGrid>
-      {IMAGES.map(i => (
-        <Link
-          key={i.id}
-          to={{
-            pathname: `/img/${i.id}`,
-            // this is the trick!
-            state: { modal: true }
-          }}
-        >
-          <Image index={i.id} />
-        </Link>
-      ))}
-    </PhotoGrid>
-  );
-}
-
 function ImageView({ match }) {
-  let image = IMAGES[parseInt(match.params.id, 10) - 1]; // why do I subtract one?
+  let image = Books[parseInt(match.params.id, 10) - 1]; // why do I subtract one?
 
   if (!image) return <div>Image not found</div>;
 
@@ -110,50 +81,6 @@ function ImageView({ match }) {
     <div>
       <h1>{image.title}</h1>
       <Image index={image.id} />
-    </div>
-  );
-}
-
-function Modal({ match, history }) {
-  let image = IMAGES[parseInt(match.params.id, 10) - 1];
-
-  if (!image) return null;
-
-  let back = e => {
-    e.stopPropagation();
-    history.goBack();
-  };
-
-  return (
-    <div
-      onClick={back}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        background: "rgba(0, 0, 0, 0.15)"
-      }}
-    >
-      <div
-        className="modal"
-        style={{
-          position: "absolute",
-          background: "#fff",
-          top: 25,
-          left: "10%",
-          right: "10%",
-          padding: 15,
-          border: "2px solid #444"
-        }}
-      >
-        <h1>{image.title}</h1>
-        <Image index={image.id} />
-        <button type="button" onClick={back}>
-          Close
-        </button>
-      </div>
     </div>
   );
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderGridStyled = styled.div`
+  display: grid;
+  justify-content: center;
+  margin: 80px 60px 50px 50px;
+`;
+
+const Header = () => (
+  <HeaderGridStyled>
+    <p>
+      While happily ignoring when being called. Eat the rubberband. Claw at
+      curtains stretch and yawn nibble on tuna ignore human bite human hand do i
+      like standing on litter cuz i sits when i have spaces, my cat buddies have
+      no litter i live in luxury cat life for that box? i can fit in that box
+      make meme, make cute face so have secret plans for rub against owner
+      because nose is wet. Need to chase tail scream at teh bath wake up human
+      for food at 4am. Eat all the power cords sit on human they not getting up
+      ever, for fat baby cat best buddy little guy and jump five feet high and
+      sideways when a shadow moves, and bite off human's toes missing until
+      dinner time. I’m so hungry i’m so hungry but ew not for that put butt in
+      owner's face. Hide when guests come over fat baby cat best buddy little
+      guy or spill litter box, scratch at owner, destroy all furniture,
+      especially couch dont wait for the storm to pass, dance in the rain and
+      chew iPad power cord, tuxedo cats always looking dapper but cry louder at
+      reflection. Eat the rubberband. Let me in let me out let me in let me out
+      let me in let me out who broke this door anyway cats are a queer kind of
+      folk. Sniff other cat's butt and hang jaw half open thereafter sleep on my
+      human's head or find something else more interesting, but give me
+      attention or face the wrath of my claws. Cry louder at reflection all of a
+      sudden cat goes crazy, spit up on light gray carpet instead of adjacent
+      linoleum pet right here, no not there, here, no fool, right here that
+      other cat smells funny you should really give me all the treats because i
+      smell the best and omg you finally got the right spot and i love you right
+      now fall asleep on the washing machine. Ignore the squirrels, you'll never
+      catch them anyway cuddle no cuddle cuddle love scratch scratch yet kitty
+      poochy. Gate keepers of hell walk on car leaving trail of paw prints on
+      hood and windshield roll on the floor purring your whiskers off fall
+      asleep upside-down and flop over, or make meme, make cute face make plans
+      to dominate world and then take a nap. Sit on human.
+    </p>
+  </HeaderGridStyled>
+);
+
+export default Header;
